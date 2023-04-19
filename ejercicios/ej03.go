74d70dddf59432956d76144eba7cec702e7d69e8
AgusLacomi <aguslacomi@gmail.com>
2023-04-19 19:45:17 -0300

    3,5 terminados

--- ejercicios/ej03.go
+++ ejercicios/ej03.go
@@ -5,5 +5,10 @@ package ejercicios
 // que es igual al número de unos en la representación binaria
 // de n/2, mas 1 si es impar.
 func CantidadDeUnos(n int) int {
-	panic("Not implemented")
+
+	if n < 2 {
+		return n
+	}
+
+	return n%2 + CantidadDeUnos(n/2)
 }
