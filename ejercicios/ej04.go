4746bb4d8927c16922c6485dc92d109bd5f6e016
AgusLacomi <aguslacomi@gmail.com>
2023-04-18 21:08:26 -0300

    1,2,4,7,9,10 terminado

--- ejercicios/ej04.go
+++ ejercicios/ej04.go
@@ -2,6 +2,10 @@ package ejercicios
 
 // Escriba un método recursivo que tome una cadena y
 // devuelva como resultado la cadena invertida.
-func Invertir(cadena string) string {
-	panic("Not implemented")
+func Invertir(palabra string) string {
+	if len(palabra) <= 1 {
+		return palabra
+	}
+
+	return Invertir(palabra[1:]) + palabra[:1]
 }
--- ejercicios/ej08.go
+++ ejercicios/ej08.go
@@ -6,3 +6,12 @@ package ejercicios
 func DivisionEntera(dividendo, divisor int) (cociente, resto int) {
 	panic("Not implemented")
 }
+
+func dividir(a, b int) int {
+
+	if a < b || b == 0 {
+		return 0
+	}
+
+	return 1 + dividir(a-b, b)
+}
